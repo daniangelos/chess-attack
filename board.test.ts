7077a4d5ededf04e1d5fba304f8f2de2dd49347d
Daniella <daniaangelos@gmail.com>
2019-04-27 14:38:30 -0300

    added test if queen cannot attack

--- board.test.ts
+++ board.test.ts
@@ -10,3 +10,18 @@ describe('Board', () => {
     expect(chessBoard.atPosition(2, 3)).toBe(queen)
   })
 })
+
+describe('Piece can attack', () => {
+  it('Queen cannot attack', () => {
+    const chessBoard = new ChessBoard()
+    const queen = new Queen(2, 3, 'black')
+    //canAttack(enemy: Piece, board: number[][])
+
+    chessBoard.addPiece(queen)
+    console.log(chessBoard.toString())
+    expect(queen.canAttack(null, chessBoard.board)).toBe(false)
+
+  })
+
+
+})
