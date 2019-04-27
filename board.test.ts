c88c6587b7b1cd2faf31a33da34d03314c5d5e92
Daniella <daniaangelos@gmail.com>
2019-04-27 15:24:12 -0300

    share line improvement

--- board.test.ts
+++ board.test.ts
@@ -35,6 +35,18 @@ describe('Piece can attack', () => {
 
   })
 
+  it('Queen can attack in line same color', () => {
+    const chessBoard = new ChessBoard()
+    const queen = new Queen(2, 3, 'black')
+    chessBoard.addPiece(queen)
+    const queen2 = new Queen(2, 4, 'black')
+    chessBoard.addPiece(queen2)
+
+    console.log(chessBoard.toString())
+    expect(queen.canAttack(queen2, chessBoard.board)).toBe(false)
+
+  })
+
   it('Queen can attack in column', () => {
     const chessBoard = new ChessBoard()
     const queen = new Queen(3, 3, 'black')
@@ -58,6 +70,35 @@ describe('Piece can attack', () => {
 
     console.log(chessBoard.toString())
     expect(queen.canAttack(queen2, chessBoard.board)).toBe(true)
+
+  })
+
+  it('Queen can attack in diagonal', () => {
+    const chessBoard = new ChessBoard()
+    const queen = new Queen(2, 2, 'black')
+    chessBoard.addPiece(queen)
+    const queen2 = new Queen(3, 1, 'white')
+    chessBoard.addPiece(queen2)
+
+
+    console.log(chessBoard.toString())
+    expect(queen.canAttack(queen2, chessBoard.board)).toBe(true)
+
+  })
+
+
+  it('Queen can attack in diagonal', () => {
+    const chessBoard = new ChessBoard()
+    const whiteQueen = new Queen(2, 2, 'white')
+    chessBoard.addPiece(whiteQueen)
+    const blackQueen = new Queen(2, 5, 'black')
+    chessBoard.addPiece(blackQueen)
+    const anotherBlackQueen = new Queen(2, 6, 'black')
+    chessBoard.addPiece(anotherBlackQueen)
+
+
+    console.log(chessBoard.toString())
+    expect(whiteQueen.canAttack(anotherBlackQueen, chessBoard.board)).toBe(false)
 
   })
 
--- src/Queen.ts
+++ src/Queen.ts
@@ -5,11 +5,15 @@ class Queen extends Piece {
 
     if (!enemy) return false
 
-    if (this.shareLine(enemy.line)) return true
+    if (enemy.color === this.color) return false
+
+    if (this.shareLine(enemy.line, enemy.column, board)) return true
 
     if (this.shareColumn(enemy.column)) return true
 
-    if (board) return true
+    if (this.shareDiagonal(enemy.line, enemy.column)) return true
+
+    console.log(board[0][0])
 
     return false
   }
@@ -17,14 +21,23 @@ class Queen extends Piece {
   shareDiagonal(line: number, column: number): boolean {
     const subLine = this.line - line
     const subColumn = this.column - column
-    if (subLine === subColumn) {
+    if (Math.abs(subLine) === Math.abs(subColumn)) {
       return true
     }
     return false
   }
 
-  shareLine(line: number): boolean {
-    return this.line === line
+  shareLine(line: number, column: number, board: number[][]): boolean {
+    const left = Math.min(this.column, column)
+    const right = Math.max(this.column, column)
+
+    if (this.line !== line) return false
+
+    for (let i = left + 1; i < right; i++) {
+      if (board[this.line][i] !== -1) return false
+    }
+
+    return true
   }
 
   shareColumn(column: number): boolean {
